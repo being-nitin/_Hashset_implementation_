e84dc9ab1e6bd6d45fb063ceb6221adda7453d5d
Nitin <nsinghboss02@gmail.com>
2022-03-06 21:55:53 +0530

    duplicate element will not be added.

--- src/com/Recursion/Main.java
+++ src/com/Recursion/Main.java
@@ -5,12 +5,19 @@ public class Main {
     public static void main(String[] args) {
 	// Hashset Implementation.
     // in this duplicate elements are not allowed
+
         HashSet <Integer> set = new HashSet<>();
         set.add(89);
         set.add(90);
         set.add(91);
         set.add(92);
         set.remove(91);
+        System.out.println(set);
+        set.add(89);
+        set.add(90);
+        /* in this 89,90 will not be considered because set will terminate the
+           duplicate element.
+         */
         System.out.println(set);
     }
 }
